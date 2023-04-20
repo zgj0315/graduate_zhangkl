9bb656cbdca5ba6872fb920d4a611cb82b53cd2a
赵光建 <zhaoguangjian@cyberkl.com>
2023-04-20 09:40:35 +0800

    cloud function works

--- miniprogram/pages/consult/index.js
+++ miniprogram/pages/consult/index.js
@@ -26,7 +26,20 @@ Page({
    * Lifecycle function--Called when page show
    */
   onShow() {
-
+    console.log('onShow')
+    wx.cloud.callFunction({
+      // 云函数名称
+      name: 'add',
+      // 传给云函数的参数
+      data: {
+        a: 1,
+        b: 2,
+      },
+      success: function(res) {
+        console.log(res.result.sum) // 3
+      },
+      fail: console.error
+    })
   },
 
   /**
