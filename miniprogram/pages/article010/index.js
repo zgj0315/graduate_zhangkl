e721e3bc32f41e4829c90dba6d21381c93e852e1
赵光建 <zhaoguangjian@cyberkl.com>
2023-04-24 10:56:18 +0800

    it works

--- miniprogram/pages/article010/index.js
+++ miniprogram/pages/article010/index.js
@@ -5,7 +5,16 @@ Page({
    * Page initial data
    */
   data: {
-
+    theme: 'light',
+    items: [
+      { value: '001', name: '轻微恶心' },
+      { value: '002', name: '呕吐' },
+      { value: '003', name: '腹胀' },
+      { value: '004', name: '腹痛' },
+      { value: '005', name: '恶心' },
+      { value: '006', name: '排气排便停止' }
+    ],
+    message: ''
   },
 
   /**
@@ -62,5 +71,36 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+  checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+
+    const items = this.data.items
+    const values = e.detail.value
+    this.setData({
+      message: ''
+    })
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      items[i].checked = false
+      for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (items[i].value === values[j]) {
+          items[i].checked = true
+          if ('001' === values[j] || '002' === values[j] || '003' === values[j]) {
+            this.setData({
+              message: '根据身体情况多活动，遵医嘱服用助排气排便药物。'
+            })
+          } else if ('004' === values[j] || '005' === values[j] || '006' === values[j]) {
+            this.setData({
+              message: '先禁食禁水，及时就医。'
+            })
+          }
+          break
+        }
+      }
+    }
+    this.setData({
+      items
+    })
   }
 })
--- miniprogram/pages/article015/index.js
+++ miniprogram/pages/article015/index.js
@@ -5,7 +5,16 @@ Page({
    * Page initial data
    */
   data: {
-
+    theme: 'light',
+    items: [
+      { value: '001', name: '轻微红肿' },
+      { value: '002', name: '硬结' },
+      { value: '003', name: '血肿' },
+      { value: '004', name: '未化脓' },
+      { value: '005', name: '裂开化脓' },
+      { value: '006', name: '脂肪液化' }
+    ],
+    message: ''
   },
 
   /**
@@ -62,5 +71,36 @@ Page({
    */
   onShareAppMessage() {
 
+  },
+
+  checkboxChange(e) {
+    console.log('checkbox发生change事件，携带value值为：', e.detail.value)
+
+    const items = this.data.items
+    const values = e.detail.value
+    this.setData({
+      message: ''
+    })
+    for (let i = 0, lenI = items.length; i < lenI; ++i) {
+      items[i].checked = false
+      for (let j = 0, lenJ = values.length; j < lenJ; ++j) {
+        if (items[i].value === values[j]) {
+          items[i].checked = true
+          if ('001' === values[j] || '002' === values[j] || '003' === values[j] || '004' === values[j]) {
+            this.setData({
+              message: '遵医嘱服用抗生素，保持伤口清洁干燥，按时换药，如未好转及时就医。'
+            })
+          } else if ('005' === values[j] || '006' === values[j]) {
+            this.setData({
+              message: '及时就医。'
+            })
+          }
+          break
+        }
+      }
+    }
+    this.setData({
+      items
+    })
   }
 })
