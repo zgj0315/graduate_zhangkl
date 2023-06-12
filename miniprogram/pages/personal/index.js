2888a58545647e3f0b1b40d49f974611e8b82302
赵光建 <zhaoguangjian@cyberkl.com>
2023-06-12 11:03:28 +0800

    it works

--- miniprogram/pages/personal/index.js
+++ miniprogram/pages/personal/index.js
@@ -7,7 +7,6 @@ Page({
   data: {
     week_list: ['日', '一', '二', '三', '四', '五', '六'],
     checkin_list: [],
-
   },
 
   /**
@@ -71,16 +70,38 @@ Page({
   },
 
   onShow() {
+    let self = this
     const db = wx.cloud.database()
     db.collection('zkf_check_in').where({
-
     }).get({
       success: function (res) {
+        var week_lasted = -1
+        var week_list = []
+        var checkin_list = []
         for (var indexData in res.data) {
           // console.log('res.data: ', res.data[indexData])
           var date = new Date(res.data[indexData].date);
-          console.log('type: ', res.data[indexData].type, ', step: ', res.data[indexData].step, ', getDay: ', date.getDay())
+          // console.log('type: ', res.data[indexData].type, ', step: ', res.data[indexData].step, ', getDay: ', date.getDay())
+          if (week_lasted <= date.getDay()) {
+            week_lasted = date.getDay()
+          } else {
+            checkin_list.push(week_list)
+            week_lasted = -1;
+            week_list = []
+          }
+          if (week_list.length == 0) {
+            var offset = date.getDay();
+            for (var i = 0; i < 7; i++) {
+              week_list.push([date.getDate() - offset + i, 0])
+            }
+          }
+          week_list[date.getDay()][1] = 1
         }
+        checkin_list.push(week_list)
+        console.log('checkin_list: ', checkin_list)
+        self.setData({
+          checkin_list: checkin_list
+        })
       },
       fail: err => {
         console.error('[数据库] [查询记录] 失败：', err)
